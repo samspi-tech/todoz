f786ea93dcf60e9e0d0aae482a95ca7cef30e9c1
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-03-31 12:45:22 +0200

    refactor: change root page name to home

--- src/react-router/router.ts
+++ src/react-router/router.ts
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from 'react-router';
 
-import Welcome from '@/pages/Welcome.tsx';
+import Home from '@/pages/Home.tsx';
 import Settings from '@/pages/Settings.tsx';
 import Tasks from '@/pages/Tasks.tsx';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        Component: Welcome,
+        Component: Home,
     },
     {
         path: '/tasks',
