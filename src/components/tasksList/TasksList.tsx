4fcca4945ce1dab019864cafcebdce6830802769
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-04-01 19:30:59 +0200

    feat(tasks-list): add modal

--- src/components/tasksList/TasksList.tsx
+++ src/components/tasksList/TasksList.tsx
@@ -1,17 +1,27 @@
 import { Plus } from 'lucide-react';
 
 import Button from '@/components/button/Button.tsx';
+import Modal from '@/components/modal/Modal.tsx';
 
 import styles from './TasksList.module.css';
+import { useModal } from '@/hooks/useModal.ts';
 
 const TasksList = () => {
+    const { dialogRef, handleOpenModal, handleCloseModal } = useModal();
+
     return (
         <section className={styles.taskListsContainer}>
             <h2>Your task lists</h2>
 
-            <Button variant="rounded">
+            <Button variant="rounded" onClick={handleOpenModal}>
                 <Plus />
             </Button>
+
+            <Modal
+                ref={dialogRef}
+                title="Create list"
+                onClose={handleCloseModal}
+            ></Modal>
         </section>
     );
 };
