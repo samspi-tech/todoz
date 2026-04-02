367b4e127bb46169ff3dbb90ce1a116beda15b32
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-04-02 19:26:19 +0200

    feat: add tasks-list-card

---
+++ src/components/tasksListCard/TasksListCard.tsx
@@ -0,0 +1,31 @@
+import { EllipsisVertical, Pin } from 'lucide-react';
+
+import styles from './TasksListCard.module.css';
+
+interface TasksListCardProps {
+    title: string;
+    footerText?: string;
+}
+
+const TasksListCard = ({ title, footerText }: TasksListCardProps) => {
+    return (
+        <article className={styles.card}>
+            <header>
+                <Pin />
+                <EllipsisVertical />
+            </header>
+
+            <div className={styles.cardBody}>
+                <h3>{title}</h3>
+            </div>
+
+            {footerText && (
+                <footer>
+                    <small>{footerText}</small>
+                </footer>
+            )}
+        </article>
+    );
+};
+
+export default TasksListCard;
