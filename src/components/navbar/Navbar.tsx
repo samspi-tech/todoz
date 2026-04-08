e514b7c5dcc41369d64bc06d4c458a63911e670f
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-04-08 12:47:21 +0200

    feat(navbar): add max-width

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,16 +7,18 @@ import styles from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className={styles.navbar}>
-            <Logo />
+        <nav className={styles.container}>
+            <div className={styles.navbar}>
+                <Logo />
 
-            <NavLink
-                to="/settings"
-                viewTransition={true}
-                aria-label="Navigate to settings page"
-            >
-                <Settings />
-            </NavLink>
+                <NavLink
+                    to="/settings"
+                    viewTransition={true}
+                    aria-label="Navigate to settings page"
+                >
+                    <Settings />
+                </NavLink>
+            </div>
         </nav>
     );
 };
