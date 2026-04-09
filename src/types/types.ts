b99b19863cae737e6e181a65e68322d009d6c845
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-04-09 12:51:19 +0200

    chore: add task types

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,14 @@ export type List = {
     id: string;
     title: string;
 };
+
+export type Task = {
+    id: string;
+    description: string;
+    quantity: number;
+};
+
+export type Tasks = {
+    listId: string;
+    tasks: Task[];
+};
