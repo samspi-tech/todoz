8b616a7516affd759dc3e75af0860e2639aa9559
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-04-09 12:53:48 +0200

    feat(tasks-deatils): add modal

--- src/components/tasksDetails/TasksDetails.tsx
+++ src/components/tasksDetails/TasksDetails.tsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from 'react';
 import { Plus } from 'lucide-react';
 
 import Button from '@/components/button/Button.tsx';
+import Modal from '@/components/modal/Modal.tsx';
 
 import { useListContext } from '@/hooks/useListContext.ts';
 import type { List } from '@/types/types.ts';
 import styles from './TasksDetails.module.css';
+import { useModal } from '@/hooks/useModal.ts';
 
 const TasksDetails = () => {
     const [list, setList] = useState<List | null>(null);
 
     const { id } = useParams();
     const { getList } = useListContext();
+    const { modalRef, handleOpenModal, handleCloseModal } = useModal();
 
     useEffect(() => {
         const listData = getList(id!);
@@ -27,10 +30,16 @@ const TasksDetails = () => {
         <section className={styles.tasksContainer}>
             <header>
                 <h2>{list?.title}</h2>
-                <Button variant="square" size="small">
+                <Button variant="square" size="small" onClick={handleOpenModal}>
                     <Plus />
                 </Button>
             </header>
+
+            <Modal
+                ref={modalRef}
+                title="Create task"
+                onClose={handleCloseModal}
+            ></Modal>
         </section>
     );
 };
