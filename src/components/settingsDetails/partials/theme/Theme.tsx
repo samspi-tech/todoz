2cf0b964e95c36c9c3cbd5cb5d4bf0bd2eef0263
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-04-23 20:40:55 +0200

    feat(settings): add theme component

--- src/components/settingsDetails/partials/theme/Theme.tsx
+++ src/components/settingsDetails/partials/theme/Theme.tsx
@@ -1,11 +1,20 @@
-import { SunMoon } from 'lucide-react';
+import { Sun } from 'lucide-react';
 
 import OptionContainer from '@/components/settingsDetails/partials/optionContainer/OptionContainer.tsx';
 
+import styles from './Theme.module.css';
+
 const Theme = () => {
     return (
-        <OptionContainer icon={<SunMoon />} label="Theme">
-            <p>Change color theme</p>
+        <OptionContainer icon={<Sun />} label="Theme">
+            <ul className={styles.themeContainer}>
+                <li className={styles.active}>
+                    <span>Light</span>
+                </li>
+                <li>
+                    <span>Dark</span>
+                </li>
+            </ul>
         </OptionContainer>
     );
 };
