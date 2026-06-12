633349116a357985c633f26a1d33250172eabc60
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-06-12 21:13:23 +0200

    chores: update pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -24,8 +24,32 @@ export default defineConfig({
                 display: 'standalone',
                 icons: [
                     {
-                        src: '/pwa-64x64.png',
-                        sizes: '64x64',
+                        src: '/pwa-72x72.png',
+                        sizes: '72x72',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                    {
+                        src: '/pwa-96x96.png',
+                        sizes: '96x96',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                    {
+                        src: '/pwa-128x128.png',
+                        sizes: '128x128',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                    {
+                        src: '/144x144.png',
+                        sizes: '144x144',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                    {
+                        src: '/pwa-152x152.png',
+                        sizes: '152x152',
                         type: 'image/png',
                         purpose: 'maskable',
                     },
